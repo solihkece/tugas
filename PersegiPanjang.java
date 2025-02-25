09e614ba055c308705df705710636957b3813c5e
solihkece <161547756+solihkece@users.noreply.github.com>
2025-02-25 15:55:45 +0700

    Add files via upload

---
+++ PersegiPanjang.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ti23e;
+
+/**
+ *
+ * @author JARKOM16
+ */
+public class PersegiPanjang {
+    
+}
